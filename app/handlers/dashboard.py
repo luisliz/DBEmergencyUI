544262821757e8d5f1211b96e861f700036ed2e4
Luis Liz <luis.liz@upr.edu>
2020-03-10 11:48:17 -0400

    initial commit

--- app/handlers/dashboard.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint
-
-dashboard_bp = Blueprint("dashboard", __name__)
-
-@dashboard_bp.route('/')
-def index():
-    return "Welcome to dashboard"
--- app/handlers/resources.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint
-
-resources_bp = Blueprint('resources', __name__)
-
-@resources_bp.route('/')
-def index():
-    return "Welcome to resources"
